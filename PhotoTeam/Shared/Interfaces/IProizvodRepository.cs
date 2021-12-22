2e79e909549bbee5841015ae4bc3a250a331f344
DESKTOP-BGH2PEG\NIKOLA <52-2019@ftn.edu.rs>
2021-12-22 16:33:24 +0100

    Kreiran ProizvodRepository i odgovarajuci interfejs, klasa Constants i metoda GetAllProducts()

---
+++ PhotoTeam/Shared/Interfaces/IProizvodRepository.cs
@@ -0,0 +1,17 @@
+﻿using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Interfaces
+{
+    public interface IProizvodRepository
+    {
+        List<Proizvod> GetAllProducts();
+        bool InsertProduct(Proizvod p);
+        bool UpdateProduct(Proizvod p);
+        bool DeleteProduct(Proizvod p);
+    }
+}
