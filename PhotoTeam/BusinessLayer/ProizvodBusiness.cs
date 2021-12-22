5c36b4bf5e624cc00dd0ff782054790223c5be51
LEGION-5\Zlatan <30-2019@ftn.edu.rs>
2021-12-22 17:05:38 +0100

    Kreirana klasa ProizvodBusiness i odgovarajuci intrefejs i metode GetAllProducts i InsertProduct

---
+++ PhotoTeam/BusinessLayer/ProizvodBusiness.cs
@@ -0,0 +1,39 @@
+﻿using DataLayer;
+using Shared.Interfaces;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class ProizvodBusiness : IProizvodBusiness 
+    {
+        private readonly ProizvodRepository proizvodRepository;
+        public ProizvodBusiness()
+        {
+            this.proizvodRepository = new ProizvodRepository();
+        }
+
+        public List<Proizvod> GetAllProducts()
+        {
+            return this.proizvodRepository.GetAllProducts();
+        }
+
+        public bool InsertProduct(Proizvod p)
+        {
+            if (this.proizvodRepository.InsertProduct(p) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
