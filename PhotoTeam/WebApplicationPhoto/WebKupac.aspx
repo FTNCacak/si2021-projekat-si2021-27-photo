51f44d69486786c39db0c955c86e6e2572d2b8f5
DESKTOP-RG4FUD1\Sreten <17-2019@ftn.edu.rs>
2021-12-23 19:13:41 +0100

    Kreirana Web aplikacija, uradjena  logika za ispis proizvoda i deo dizajna

---
+++ PhotoTeam/WebApplicationPhoto/WebKupac.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebKupac.aspx.cs" Inherits="WebApplicationPhoto.WebKupac" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <body>
+    <br /><br />
+    <h3 style="color:brown">DOBRO DOSLI! OVDE MOZETE PORUCITI PROIZVODE.</h3>
+    <br /><br />
+    <asp:ListBox ID="ListBoxProizvodiWeb" runat="server" Height="189px" Width="500px"></asp:ListBox>
+    <br />
+    <br />
+    <asp:Label ID="Label1" runat="server" Text="Sifra"></asp:Label>  <asp:TextBox ID="TextBoxSifraProizvoda" runat="server"></asp:TextBox><br /><br />
+    <asp:Label ID="Label2" runat="server" Text="Naziv"></asp:Label>  <asp:TextBox ID="TextBoxNazivProizvoda" runat="server"></asp:TextBox>
+     <br />
+    <br />  
+       <asp:Button ID="ButtonDodaj" runat="server" Text="Dodaj" />
+</body>
+
+</asp:Content>
---
+++ PhotoTeam/WebApplicationPhoto/WebKupac.aspx.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationPhoto
+{
+    public partial class WebKupac : System.Web.UI.Page
+    {
+        private readonly ProizvodBusiness proizvodBusiness;
+
+        public WebKupac()
+        {
+            this.proizvodBusiness = new ProizvodBusiness();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+        
+        protected void RefreshData()
+        {
+            List<Proizvod> listaProizvoda = this.proizvodBusiness.GetAllProducts();
+            ListBoxProizvodiWeb.Items.Clear();
+            foreach (Proizvod p in listaProizvoda)
+            {
+                ListBoxProizvodiWeb.Items.Add(string.Format("{0} - {1} - {2} {3}", p.sifra_proizvoda,p.naziv, p.marka, p.cena));
+            }
+        }
+    }
+}
