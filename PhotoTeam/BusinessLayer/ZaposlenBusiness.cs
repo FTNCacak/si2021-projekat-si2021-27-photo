83d094a406063eceda2e85bdb776e4fd16998745
LEGION-5\Zlatan <30-2019@ftn.edu.rs>
2021-12-27 19:03:34 +0100

    Kreirana ZaposlenBusiness klasa i IZaposlenBusiness interfejs

---
+++ PhotoTeam/BusinessLayer/ZaposlenBusiness.cs
@@ -0,0 +1,25 @@
+﻿using DataLayer;
+using Shared.Interfaces;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class ZaposlenBusiness : IZaposlenBusiness
+    {
+        private readonly ZaposlenRepository zaposlenRepository;
+        public ZaposlenBusiness()
+        {
+            this.zaposlenRepository = new ZaposlenRepository();
+        }
+
+        public List<Zaposlen> GetAllEmployees()
+        {
+            return this.zaposlenRepository.GetAllEmployees();
+        }
+    }
+}
