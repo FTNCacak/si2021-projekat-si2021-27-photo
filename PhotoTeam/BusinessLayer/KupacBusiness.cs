87097b9e942dedce238cd3cd7e4799489a18f721
LEGION-5\Zlatan <30-2019@ftn.edu.rs>
2021-12-28 12:55:37 +0100

    Kreirana klasa KupacBusiness i interfejs IKupacBusiness

---
+++ PhotoTeam/BusinessLayer/KupacBusiness.cs
@@ -0,0 +1,25 @@
+﻿using DataLayer;
+using Shared.Interfaces;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class KupacBusiness : IKupacBusiness
+    {
+        private readonly KupacRepository kupacRepository;
+        public KupacBusiness()
+        {
+            this.kupacRepository = new KupacRepository();
+        }
+
+        public List<Proizvod> GetAllProducts()
+        {
+            return this.kupacRepository.GetAllProducts();
+        }
+    }
+}
