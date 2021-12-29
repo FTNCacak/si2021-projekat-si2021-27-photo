6423bac2456dc7da0ada48b9bf6c4554c0955bd1
DESKTOP-GUOFQTV\Korisnik <59-2019@ftn.edu.rs>
2021-12-29 13:26:47 +0100

    Odradjeni Regex izrazi na sloju prezentacije

--- PhotoTeam/PresentationLayer/FormaProdavac.cs
+++ PhotoTeam/PresentationLayer/FormaProdavac.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -37,16 +38,57 @@ namespace PresentationLayer
 
         private void button_Dodaj_Click(object sender, EventArgs e)
         {
+            if (textBox_Naziv.Text == "" || textBox_Marka.Text == "" || textBox_Cena.Text == "" || textBox_Garancija.Text == "")
+            {
+                MessageBox.Show("Morate popuniti sva polja!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox_Naziv.Focus();
+                return;
+            }
+
+             // Proveriti da li je naziv tacno unet (tačno jedna ili dve reci)
+
+            if (!Regex.Match(textBox_Naziv.Text,@"^([a-zA-Z]+|[a-zA-Z]+\s[a-zA-Z]+)$").Success)
+            {
+                MessageBox.Show("Naziv nije pravilno unet!", "Message",
+               MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Proveriti da li je marka tacno uneta(pocetno slovo malo ili veliko ostala mala ili velika
+            // i moze sadrzati razmak i cifre)
+           if (!Regex.Match(textBox_Marka.Text, @"^([a-zA-Z][a-zA-Z]*)+\s*\d*").Success)
+           {
+                MessageBox.Show("Marka nije pravilno uneta!", "Message",
+                 MessageBoxButtons.OK, MessageBoxIcon.Error);
+               return;
+           }
+            // Proveriti da li je cena tacno uneta(mora sadzati cifre, tacka nije obavezna)
+            if (!Regex.Match(textBox_Cena.Text, @"\d$[.]*").Success)
+            {
+                MessageBox.Show("Cena nije pravilno uneta!", "Message",
+                 MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            // Proveriti da li je garancija tacno uneta(mora sadzati samo cifre, bez drugih karaktera,samo cele brojeve)
+            if (!Regex.Match(textBox_Garancija.Text, "^[0-9]+$").Success)
+            {
+                MessageBox.Show("Garancija nije pravilno uneta!", "Message",
+                 MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             Proizvod p = new Proizvod();
             p.naziv = textBox_Naziv.Text;
             p.marka = textBox_Marka.Text;
             p.cena = Convert.ToDecimal(textBox_Cena.Text);
             p.garancija = Convert.ToInt32(textBox_Garancija.Text);
 
+           
+
             if (this.proizvodBusiness.InsertProduct(p))
             {
                 RefreshData();
-                MessageBox.Show("Uspesno ste uneli podatke! ", MessageBoxIcon.Information.ToString());
+                MessageBox.Show("Uspesno ste uneli podatke! ","Message",MessageBoxButtons.OK,MessageBoxIcon.Information);
                 textBox_Naziv.Clear();
                 textBox_Marka.Clear();
                 textBox_Cena.Clear();
@@ -54,7 +96,7 @@ namespace PresentationLayer
             }
             else
             {
-                MessageBox.Show("Neuspesan unos podataka!", MessageBoxIcon.Error.ToString());
+                MessageBox.Show("Neuspesan unos podataka!","Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -70,6 +112,45 @@ namespace PresentationLayer
 
         private void button_Izmeni_Click(object sender, EventArgs e)
         {
+            if (textBox_Naziv.Text == "" || textBox_Marka.Text == "" || textBox_Cena.Text == "" || textBox_Garancija.Text == "")
+            {
+                MessageBox.Show("Morate popuniti sva polja!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox_Naziv.Focus();
+                return;
+            }
+
+            // Proveriti da li je naziv tacno unet (tačno jedna ili dve reci)
+
+            if (!Regex.Match(textBox_Naziv.Text, @"^([a-zA-Z]+|[a-zA-Z]+\s[a-zA-Z]+)$").Success)
+            {
+                MessageBox.Show("Naziv nije pravilno unet!", "Message",
+               MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Proveriti da li je marka tacno uneta(pocetno slovo malo ili veliko ostala mala ili velika
+            // i moze sadrzati razmak i cifre)
+            if (!Regex.Match(textBox_Marka.Text, @"^([a-zA-Z][a-zA-Z]*)+\s*\d*").Success)
+            {
+                MessageBox.Show("Marka nije pravilno uneta!", "Message",
+                 MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            // Proveriti da li je cena tacno uneta(mora sadzati cifre, tacka nije obavezna)
+            if (!Regex.Match(textBox_Cena.Text, @"\d$[.]*").Success)
+            {
+                MessageBox.Show("Cena nije pravilno uneta!", "Message",
+                 MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            // Proveriti da li je garancija tacno uneta(mora sadzati samo cifre, bez drugih karaktera,samo cele brojeve)
+            if (!Regex.Match(textBox_Garancija.Text, "^[0-9]+$").Success)
+            {
+                MessageBox.Show("Garancija nije pravilno uneta!", "Message",
+                 MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             Proizvod p = new Proizvod();
             p.naziv = textBox_Naziv.Text;
             p.marka = textBox_Marka.Text;
@@ -81,7 +162,7 @@ namespace PresentationLayer
             if (this.proizvodBusiness.UpdateProduct(p))
             {
                 RefreshData();
-                MessageBox.Show("Uspesno ste izmenili podatke! ", MessageBoxIcon.Information.ToString());
+                MessageBox.Show("Uspesno ste izmenili podatke! ", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 textBox_Naziv.Clear();
                 textBox_Marka.Clear();
                 textBox_Cena.Clear();
@@ -89,12 +170,18 @@ namespace PresentationLayer
             }
             else
             {
-                MessageBox.Show("Neuspesna izmena podataka!", MessageBoxIcon.Error.ToString());
+                MessageBox.Show("Neuspesna izmena podataka!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void button_Obrisi_Click(object sender, EventArgs e)
         {
+            if (textBox_Naziv.Text == "" || textBox_Marka.Text == "" || textBox_Cena.Text == "" || textBox_Garancija.Text == "")
+            {
+                MessageBox.Show("Morate kliknuti na proizvod koji zelite da obrisete da bi se popunila sva polja!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             Proizvod p = new Proizvod();
             p.naziv = textBox_Naziv.Text;
             p.marka = textBox_Marka.Text;
@@ -106,7 +193,7 @@ namespace PresentationLayer
             if (this.proizvodBusiness.DeleteProduct(p))
             {
                 RefreshData();
-                MessageBox.Show("Uspesno ste obrisali podatak! ", MessageBoxIcon.Information.ToString());
+                MessageBox.Show("Uspesno ste obrisali podatak! ", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 textBox_Naziv.Clear();
                 textBox_Marka.Clear();
                 textBox_Cena.Clear();
@@ -114,7 +201,7 @@ namespace PresentationLayer
             }
             else
             {
-                MessageBox.Show("Niste obrisali podatak!", MessageBoxIcon.Error.ToString());
+                MessageBox.Show("Niste obrisali podatak!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
